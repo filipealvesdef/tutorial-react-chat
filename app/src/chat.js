91d1944ae8e430cc70dc254563b86b46710ce89a
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2018-08-23 15:09:30 -0300

    Differentiate my messages from her messages

--- app/src/chat.js
+++ app/src/chat.js
@@ -3,8 +3,19 @@ import React from 'react';
 import './chat.css';
 
 class ChatMessage extends React.Component {
+    static defaultProps = {
+        fromMe: false,
+    };
+
     render() {
-        return <div className='chat__message'>
+        let className = 'chat__message'
+        if (this.props.fromMe) {
+            className += ' chat__message--from-me';
+        } else {
+            className += ' chat__message--from-her';
+        }
+
+        return <div className={className}>
             {this.props.children}
         </div>;
     }
@@ -14,19 +25,19 @@ class Chat extends React.Component {
     render() {
         return <div className='chat'>
             <div className='chat__log'>
-                <ChatMessage>
+                <ChatMessage fromMe>
                     Hi.
                 </ChatMessage>
                 <ChatMessage>
                     Hello.
                 </ChatMessage>
-                <ChatMessage>
+                <ChatMessage fromMe>
                     How are you?
                 </ChatMessage>
                 <ChatMessage>
                     I'm good. How about you?
                 </ChatMessage>
-                <ChatMessage>
+                <ChatMessage fromMe>
                     I'm fine.
                 </ChatMessage>
             </div>
