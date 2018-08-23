528d70605812336d44c9babdfe73eb023640b324
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2018-08-23 14:51:17 -0300

    Add initial version of <Chat>

---
+++ app/src/chat.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './chat.css';
+
+class Chat extends React.Component {
+    render() {
+        return <div className='chat'>
+            <div className='chat__log'>
+                <div className='chat__message'>
+                    Hi.
+                </div>
+                <div className='chat__message'>
+                    Hello.
+                </div>
+                <div className='chat__message'>
+                    How are you?
+                </div>
+                <div className='chat__message'>
+                    I'm good. How about you?
+                </div>
+                <div className='chat__message'>
+                    I'm fine.
+                </div>
+            </div>
+            <div className='chat__input'>
+            </div>
+        </div>;
+    }
+}
+
+export default Chat;
