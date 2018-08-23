09fd7a515562f0c2502abac1be8898403deb2030
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2018-08-23 14:51:47 -0300

    Use <Chat> in <App>

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './app.css';
 
+import Chat from './chat';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Chat />
       </div>
     );
   }
