b49d98b2b14d29a81ca53b3869fffcc43469e524
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2019-08-29 10:22:42 -0300

    Differentiate my messages from her messages
    Notes:
        - Introduction of BEM modifier (--)
        - Introduction of defaultProps
        - Defined in the element without explict value are true by default

--- src/chat.js
+++ src/chat.js
@@ -3,27 +3,39 @@ import React from 'react';
 import './chat.css';
 
 function ChatMessage(props) {
-    return <div className='chat__message'>
+    let className = 'chat__message';
+
+    if (props.fromMe) {
+        className += ' chat__message--from-me';
+    } else {
+        className += ' chat__message--from-her';
+    }
+
+    return <div className={className}>
       {props.children}
     </div>;
 }
 
+ChatMessage.defaultProps = {
+    fromMe: false,
+};
+
 function Chat() {
     return <div className='chat'>
       <div className='chat__log'>
-        <ChatMessage>
+        <ChatMessage fromMe>
           Hi.
         </ChatMessage>
         <ChatMessage>
           Hello.
         </ChatMessage>
-        <ChatMessage>
+        <ChatMessage fromMe>
           How are you?
         </ChatMessage>
         <ChatMessage>
           I'm good. How about you?
         </ChatMessage>
-        <ChatMessage>
+        <ChatMessage fromMe>
           I'm fine.
         </ChatMessage>
       </div>
