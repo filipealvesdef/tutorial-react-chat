dd070999b5f0fbabb673495b08c1e74205131502
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-09-13 07:30:58 -0300

    Add agent prop (buggy version)

--- src/app.js
+++ src/app.js
@@ -4,12 +4,25 @@ import { Chat, ChatMessage } from './chat.js';
 
 import './app.css';
 
-function App() {
+function App(props) {
     const [messages, setMessages] = useState([]);
 
     function onSend(userInput) {
         const msg = {from: 'me', text: userInput}
         setMessages(messages.concat([msg]));
+
+        props.agent.sendMessage(userInput).then(response => {
+            if (!response) {
+                return;
+            }
+
+            const msgs = response.map(text => ({
+                from: 'her',
+                text,
+            }));
+
+            setMessages(messages.concat(msgs))
+        })
     }
 
     return <Chat onSend={onSend}>
@@ -19,6 +32,26 @@ function App() {
             children={m.text}
         />)}
     </Chat>;
+}
+
+App.defaultProps = {
+    agent: {
+        state: 'idle',
+        sendMessage(userInput) {
+            if (this.state === 'idle') {
+                this.state = 'recording'
+                return Promise.resolve([
+                    'Hello I\'m an answering machine. Leave your message after the beep.',
+                    'BEEEEEP....',
+                ]);
+            } else {
+                this.state = 'idle'
+                return Promise.resolve([
+                    'Message recorded...not really',
+                ])
+            }
+        }
+    }
 }
 
 export default App;
